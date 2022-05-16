892791afeefcc95e439fc0b755419a2411ee5536
moirawassem <mwassem@cshnyc.org>
2022-05-16 14:54:06 -0400

    updated project 5/16

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+createCanvas(windowWidth, windowHeight);
+}
+function draw(){
+  background(255);
+noStroke();
+fill(305,200,200);
+rect(500,100,400,200);
+  fill('black');
+textSize(25);
+  text("Resume of a Brilliant Student", 450, 100)
+  text("...MOIRA WASSEM", 470, 140);
+  fill('pink');
+  rect(0,0,100,70);
+
+
+
+
+}
