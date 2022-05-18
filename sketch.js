1999d8d2d44358135fc85dc09134e3921e19b21d
moirawassem <mwassem@cshnyc.org>
2022-05-18 08:41:47 -0400

    updated 5/18

--- sketch.js
+++ sketch.js
@@ -4,14 +4,12 @@ createCanvas(windowWidth, windowHeight);
 function draw(){
   background(255);
 noStroke();
-fill(305,200,200);
-rect(500,100,400,200);
+fill('pink');
+rect(470,50,400,150);
   fill('black');
 textSize(25);
-  text("Resume of a Brilliant Student", 450, 100)
-  text("...MOIRA WASSEM", 470, 140);
-  fill('pink');
-  rect(0,0,100,70);
+  text("Resume of a Brilliant Student", 500, 100)
+  text("...MOIRA WASSEM", 550, 140);
 
 
 
