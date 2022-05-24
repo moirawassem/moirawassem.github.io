3335d8e02a620fa57be5a66bbafc33d3eaa1e659
moirawassem <mwassem@cshnyc.org>
2022-05-24 14:58:56 -0400

    new updated

--- sketch.js
+++ sketch.js
@@ -4,12 +4,22 @@ createCanvas(windowWidth, windowHeight);
 function draw(){
   background(255);
 noStroke();
+fill(35, 78, 196);
+rect(400,30,560,160);
 fill('pink');
-rect(470,50,400,150);
+rect(430,50,505,120);
   fill('black');
 textSize(25);
-  text("Resume of a Brilliant Student", 500, 100)
-  text("...MOIRA WASSEM", 550, 140);
+  text("A Collection of Projects by a Brilliant Student", 435, 100)
+  text("...MOIRA WASSEM", 560, 140);
+  textSize(15);
+  textStyle(ITALIC);
+  text('"The greatest coder of this generation."--Ms. Mitchell', 500, 300);
+  text('"She hacked into my heart this year. A strong young woman in stem."--Ms. Sousa', 430, 340);
+  text('"A true genius, but a humble one."--Dr.Lim',490, 380);
+  text('"I bore a prodigy."---My Mom',520, 420)
+  text('"The cutest coder in the game."--Cara Neary', 490, 460)
+  text('"A force of nature."--Andrea Molina', 500, 500)
 
 
 
