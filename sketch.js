2c019769388196dd5207bac8a35206b831d87a4d
moirawassem <mwassem@cshnyc.org>
2022-05-26 08:47:16 -0400

    New update

--- sketch.js
+++ sketch.js
@@ -12,15 +12,24 @@ rect(430,50,505,120);
 textSize(25);
   text("A Collection of Projects by a Brilliant Student", 435, 100)
   text("...MOIRA WASSEM", 560, 140);
+  textSize(20);
+  text('Some References...', 580, 245);
+  textSize(10)
+  text('*Note quotes may have been adjusted for dramatic effect*', 540, 275);
   textSize(15);
   textStyle(ITALIC);
   text('"The greatest coder of this generation."--Ms. Mitchell', 500, 300);
-  text('"She hacked into my heart this year. A strong young woman in stem."--Ms. Sousa', 430, 340);
-  text('"A true genius, but a humble one."--Dr.Lim',490, 380);
-  text('"I bore a prodigy."---My Mom',520, 420)
-  text('"The cutest coder in the game."--Cara Neary', 490, 460)
-  text('"A force of nature."--Andrea Molina', 500, 500)
-
+  text('"She hacked into my heart this year. A strong young woman in stem."--Ms. Sousa', 440, 340);
+  text('"A true genius, but a humble one."--Dr.Lim',520, 380);
+  text('"I bore a prodigy."---My Mom',550, 420)
+  text('"The cutest coder in the game."--Cara Neary', 530, 460)
+  text('"A force of nature."--Andrea Molina', 540, 500)
+fill('pink');
+  rect(960,150,270,110);
+  fill('black');
+  textSize(20);
+text('Follow me on github ;)',1000, 200);
+text('@moirawassem', 1000, 220);
 
 
 
